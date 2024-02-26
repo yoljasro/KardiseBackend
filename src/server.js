b988c6029abba8313d4a03d8b6357f9dee93cb7d
yoljasro <saidaliyevjasur450@gmail.com>
2024-02-26 20:07:45 +0500

    change

--- src/server.js
+++ src/server.js
@@ -38,6 +38,8 @@ const run = async () => {
 
   app.use(admin.options.rootPath, router);
   app.use(express.json());
+  app.use(express.json({ limit: '100mb' }));
+  app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
   app.use('/uploads', express.static('uploads'));
 
